8172e40db737470ab9c5a0601b5535ece71daf8b
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-05 23:04:52 +0530

    feat: add weather detail elements to display additional information

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,4 +49,21 @@ let apiCall = async (city) => {
 let setWeather = (data) =>{
     let city = document.getElementById("city_name");
     let country = document.getElementById("country_name");
+
+let main_temparature = document.getElementById("main_temparature");
+    let sub_main_temparature = document.getElementById("sub_main_temparature");
+    let temparature_feel_like = document.getElementById("temparature_feel_like");
+    
+    
+    let status = document.getElementById("status");
+    let main_status_image = document.getElementById("main_status_image");
+    let date = document.getElementById("date");
+
+    // Other weather details
+    let wind_speed_value = document.getElementById("wind_speed_value");
+    let humidity_value = document.getElementById("humidity_value");
+    let cloud_cover_value = document.getElementById("cloud_cover_value");
+    let uv_index_value = document.getElementById("uv_index_value");
+    let pressure_value = document.getElementById("pressure_value");
+    let visibility_value = document.getElementById("visibility_value");
 }
